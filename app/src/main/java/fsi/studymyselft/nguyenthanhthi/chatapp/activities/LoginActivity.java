1ae5c5cdd678441c6c456bf6f15c42f64fdfcd31
ThanhThiuet <thanhthiuet@gmail.com>
2018-05-16 10:31:18 +0700

    Set automatic login - change day 16-05-2018

--- app/src/main/java/fsi/studymyselft/nguyenthanhthi/chatapp/activities/LoginActivity.java
+++ app/src/main/java/fsi/studymyselft/nguyenthanhthi/chatapp/activities/LoginActivity.java
@@ -17,8 +17,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 import fsi.studymyselft.nguyenthanhthi.chatapp.R;
+import fsi.studymyselft.nguyenthanhthi.chatapp.data.User;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -29,6 +38,10 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private FirebaseAuth.AuthStateListener authStateListener;
     private FirebaseUser currentUser;
+    private FirebaseDatabase database;
+    private DatabaseReference rootReference, usersReference;
+
+    private ArrayList<User> userList;
 
     private final String TAG = "LoginActivity";
 
@@ -42,7 +55,12 @@ public class LoginActivity extends AppCompatActivity {
         buttonLogin = findViewById(R.id.btn_login);
         goToRegister = findViewById(R.id.register);
 
+        userList = new ArrayList<>();
+
         auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        rootReference = database.getReference();
+        usersReference = rootReference.child("Users");
 
         //go to Register Activity
         goToRegister.setOnClickListener(new View.OnClickListener() {
@@ -59,6 +77,31 @@ public class LoginActivity extends AppCompatActivity {
                 //input email and password of user
                 String inputEmail = edtEmail.getText().toString().trim();
                 String inputPass = edtPassword.getText().toString().trim();
+
+                //automatic login - set default email and password
+                if (inputEmail.isEmpty() && inputPass.isEmpty()) {
+                    //check database
+                    if (usersReference == null) {
+                        Toast.makeText(LoginActivity.this, "Database Users have not created!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    //push data users to userList
+                    pushDataUsersToList();
+                    int total = userList.size(); //total user records in database Users
+
+                    //check total users in database
+                    if (total == 0) {
+//                        Toast.makeText(LoginActivity.this, "Do not have data users in database!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    Random rd = new Random(total);
+                    int index = rd.nextInt(total);
+                    User user = userList.get(index);
+                    inputEmail = user.getEmail();
+                    inputPass = "123456";
+                }
 
                 if (!hasError(inputEmail, inputPass)) {
                     loginWithEmailPassword(inputEmail, inputPass);
@@ -85,6 +128,26 @@ public class LoginActivity extends AppCompatActivity {
 //        };
     }
 
+    private void pushDataUsersToList() {
+        usersReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    userList.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        User user = snapshot.getValue(User.class);
+                        userList.add(user);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -99,7 +162,7 @@ public class LoginActivity extends AppCompatActivity {
 //        }
     }
 
-    private void loginWithEmailPassword(String email, String password) {
+    private void loginWithEmailPassword(final String email, String password) {
         auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -107,6 +170,9 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             Log.d(TAG, "signInWithEmailPassword:success:");
                             Toast.makeText(LoginActivity.this, "Login successfully!", Toast.LENGTH_SHORT).show();
+
+                            //show greeting
+                            Toast.makeText(LoginActivity.this, "Wellcome " + email.toString(), Toast.LENGTH_SHORT).show();
 
                             //go to List Users Activity
                             startActivity(new Intent(LoginActivity.this, ListUserActivity.class));
