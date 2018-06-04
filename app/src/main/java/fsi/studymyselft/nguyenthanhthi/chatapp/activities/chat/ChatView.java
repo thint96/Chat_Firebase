6c33f8e6ea1ba217de09ce0b6b7af43eccf2afb3
ThanhThiuet <thanhthiuet@gmail.com>
2018-06-04 14:27:33 +0700

    fix show message realtime success

--- app/src/main/java/fsi/studymyselft/nguyenthanhthi/chatapp/activities/chat/ChatView.java
+++ app/src/main/java/fsi/studymyselft/nguyenthanhthi/chatapp/activities/chat/ChatView.java
@@ -1,12 +1,13 @@
 package fsi.studymyselft.nguyenthanhthi.chatapp.activities.chat;
 
 import fsi.studymyselft.nguyenthanhthi.chatapp.activities.BaseMainView;
+import fsi.studymyselft.nguyenthanhthi.chatapp.activities.BaseView;
 
 /**
  * Created by thanhthi on 28/05/2018.
  */
 
-public interface ChatView extends BaseMainView {
+public interface ChatView extends BaseView {
 
     void deleteMessage();
 
