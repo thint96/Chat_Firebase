ffe19d9916e03bbdb032dacc82c5479f1036e2af
ThanhThiuet <thanhthiuet@gmail.com>
2018-06-11 16:32:13 +0700

    3rd add navigation drawer - show image avatar and custom navigation header

--- app/src/main/java/fsi/studymyselft/nguyenthanhthi/chatapp/activities/BaseMainActivity.java
+++ app/src/main/java/fsi/studymyselft/nguyenthanhthi/chatapp/activities/BaseMainActivity.java
@@ -26,12 +26,17 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
-import com.stfalcon.chatkit.commons.ImageLoader;
 
 import fsi.studymyselft.nguyenthanhthi.chatapp.R;
 import fsi.studymyselft.nguyenthanhthi.chatapp.activities.authen.login.LoginActivity;
 import fsi.studymyselft.nguyenthanhthi.chatapp.activities.listUser.ListUserActivity;
+import fsi.studymyselft.nguyenthanhthi.chatapp.data.model.User;
 
 /**
  * Created by thanhthi on 03/06/2018.
@@ -49,34 +54,81 @@ public abstract class BaseMainActivity extends BaseActivity
     private Toolbar toolbar;
     private Menu drawerMenu;
 
-    private FirebaseAuth auth;
+    private ImageView imgAvatar;
+    private TextView txtUserName, txtEmail;
+    private User myUser;
+
     private FirebaseUser currentUser;
+    private DatabaseReference rootReference, usersReference;
 
     protected void onCreateNavigationDrawer() {
         super.setContentView(R.layout.layout_base_main);
 
         Log.d(TAG, "onCreateNavigationDrawer()");
 
-        auth = FirebaseAuth.getInstance();
-        currentUser = auth.getCurrentUser();
-
         myContentLayout = (FrameLayout) findViewById(R.id.my_content_layout);
         navigationView = (NavigationView) findViewById(R.id.navigation_view);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout); // The base layout that contains your navigation drawer
 
+        //start setting header of navigation drawer
         View header = navigationView.getHeaderView(0);
-        ImageView imgAvatar = header.findViewById(R.id.img_avatar);
-        TextView txtUserName = header.findViewById(R.id.txtUserName);
-        TextView txtEmail = header.findViewById(R.id.txtEmail);
+        final ImageView imgAvatar = header.findViewById(R.id.img_avatar);
+        final TextView txtUserName = header.findViewById(R.id.txtUserName);
+        final TextView txtEmail = header.findViewById(R.id.txtEmail);
 
-        String email = currentUser.getEmail();
-        int indexEnd = email.indexOf("@");
-        txtUserName.setText(email.substring(0, indexEnd));
-        txtEmail.setText(email);
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        myUser = new User(currentUser.getUid(), currentUser.getEmail());
+
+        rootReference = FirebaseDatabase.getInstance().getReference();
+        if (rootReference.child(getString(R.string.USERS_DATABASE)) == null) {
+            rootReference.setValue(getString(R.string.USERS_DATABASE));
+        }
+        usersReference = rootReference.child(getString(R.string.USERS_DATABASE));
+
+        usersReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        User user = snapshot.getValue(User.class);
+                        Log.e(TAG, "user element: " + user.getId() + " - " + user.getEmail() + " - " + user.getAvatar());
+                        if (user.getId().equals(currentUser.getUid())) {
+                            myUser.setAvatar(user.getAvatar());
+                            Log.e(TAG, myUser.getId() + " - " + myUser.getEmail() + " - " + myUser.getAvatar());
+
+                            //set name for my user
+                            String email = myUser.getEmail();
+                            Log.e(TAG, "email: " + email);
+                            int indexEnd = email.indexOf("@");
+                            Log.e(TAG, "index end = " + indexEnd);
+                            txtUserName.setText(email.substring(0, indexEnd));
+                            txtEmail.setText(email);
+
+                            //set email for my user
+                            txtEmail.setText(email);
+
+                            //set image for avatar of my user
+                            Log.e(TAG, "Avatar url of my user: " + myUser.getAvatar());
+                            Picasso.with(getContext())
+                                    .load(myUser.getAvatar())
+                                    .resize(100, 100)
+                                    .into(imgAvatar);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        //end setting header of navigation drawer
 
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0) {
             @Override
-            public void onDrawerClosed(View v){
+            public void onDrawerClosed(View v) {
                 getSupportActionBar().show();
                 super.onDrawerClosed(v);
             }
@@ -182,8 +234,7 @@ public abstract class BaseMainActivity extends BaseActivity
     public void onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
-        }
-        else {
+        } else {
             super.onBackPressed();
         }
     }
@@ -201,5 +252,42 @@ public abstract class BaseMainActivity extends BaseActivity
                         finish();
                     }
                 });
+    }
+
+    private void setInfoOfMyUser() {
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        myUser = new User(currentUser.getUid(), currentUser.getEmail());
+
+        rootReference = FirebaseDatabase.getInstance().getReference();
+        if (rootReference.child(getString(R.string.USERS_DATABASE)) == null) {
+            rootReference.setValue(getString(R.string.USERS_DATABASE));
+        }
+        usersReference = rootReference.child(getString(R.string.USERS_DATABASE));
+
+        Log.e(TAG, "users reference: " + usersReference.toString());
+
+        usersReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        User user = snapshot.getValue(User.class);
+                        Log.e(TAG, "user element: " + user.getId() + " - " + user.getEmail() + " - " + user.getAvatar());
+                        if (user.getId().equals(currentUser.getUid())) {
+                            myUser.setAvatar(user.getAvatar());
+                            Log.e(TAG, myUser.getId() + " - " + myUser.getEmail() + " - " + myUser.getAvatar());
+                            break;
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        Log.e(TAG, myUser.getId() + " - " + myUser.getEmail() + " - " + myUser.getAvatar());
     }
 }
